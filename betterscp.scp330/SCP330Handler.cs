7024b9dd41c2ed7e977c2fc3ddb17ddfca8d0a22
Tajemniczy <SFBreakTF2@gmail.com>
2022-03-18 14:33:37 +0100

    candy yum yum

--- betterscp.scp330/SCP330Handler.cs
+++ betterscp.scp330/SCP330Handler.cs
@@ -4,7 +4,13 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
+using Exiled.API.Features.Items;
+using InventorySystem.Items.Usables.Scp330;
 using Mistaken.API.Diagnostics;
+using Scp914;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Mistaken.BetterSCP.SCP330
 {
@@ -19,12 +25,108 @@ namespace Mistaken.BetterSCP.SCP330
 
         public override void OnEnable()
         {
-
+            Exiled.Events.Handlers.Scp914.UpgradingItem += Scp914_UpgradingItem;
         }
 
         public override void OnDisable()
         {
+            Exiled.Events.Handlers.Scp914.UpgradingItem -= Scp914_UpgradingItem;
+        }
 
+        readonly List<CandyKindID> CandyList = new List<CandyKindID>()
+        {
+            CandyKindID.Yellow,
+            CandyKindID.Green,
+            CandyKindID.Red,
+            CandyKindID.Purple,
+            CandyKindID.Blue,
+            CandyKindID.Rainbow,
+            CandyKindID.Pink,
+        };
+
+        private void Scp914_UpgradingItem(Exiled.Events.EventArgs.UpgradingItemEventArgs ev)
+        {
+            if (!(ev.Item.Base is Scp330Pickup scp330))
+            {
+                return;
+            }
+
+            switch (ev.KnobSetting)
+            {
+                case Scp914KnobSetting.Rough: case Scp914KnobSetting.Coarse: ev.Item.Destroy(); break;
+                case Scp914KnobSetting.OneToOne:
+                    {
+                        if (UnityEngine.Random.Range(0, 100) <= 35)
+                        {
+                            ev.Item.Destroy();
+                            break;
+                        }
+                        List<CandyKindID> candies = Enum.GetValues(typeof(CandyKindID)).ToArray<CandyKindID>().ToList();
+                        candies.ShuffleList();
+                        scp330.ExposedCandy = candies.First(x => x != CandyKindID.Pink && x != CandyKindID.None);
+                        break;
+                    }
+                case Scp914KnobSetting.Fine:
+                    {
+                        if (UnityEngine.Random.Range(0, 100) <= 35)
+                        {
+                            ev.Item.Destroy();
+                            break;
+                        }
+                        int index = CandyList.IndexOf(scp330.ExposedCandy) + 1;
+                        int random = UnityEngine.Random.Range(0, 100);
+                        if (CandyList[index] == CandyKindID.Rainbow)
+                        {
+                            if (random >= 10)
+                            {
+                                scp330.ExposedCandy = CandyList[index];
+                            }
+                            else if (random <= 11 && random >= 50)
+                            {
+                                ev.Item.Destroy();
+                                Item pain = new Item(ItemType.Painkillers);
+                                pain.Spawn(ev.OutputPosition);
+                            }
+                            else if (random <= 51)
+                            {
+                                ev.Item.Destroy();
+                            }
+                            break;
+                        }
+                        scp330.ExposedCandy = CandyList[index];
+                        break;
+                    }
+                case Scp914KnobSetting.VeryFine:
+                    {
+                        if (UnityEngine.Random.Range(0, 100) <= 50)
+                        {
+                            ev.Item.Destroy();
+                            break;
+                        }
+                        int index = CandyList.IndexOf(scp330.ExposedCandy) + 1;
+                        int random = UnityEngine.Random.Range(0, 100);
+                        if (CandyList[index] == CandyKindID.Rainbow)
+                        {
+                            if (random >= 5)
+                            {
+                                scp330.ExposedCandy = CandyList[index];
+                            }
+                            else if (random <= 6 && random >= 35)
+                            {
+                                ev.Item.Destroy();
+                                Item pain = new Item(ItemType.Painkillers);
+                                pain.Spawn(ev.OutputPosition);
+                            }
+                            else if (random <= 36)
+                            {
+                                ev.Item.Destroy();
+                            }
+                            break;
+                        }
+                        scp330.ExposedCandy = CandyList[index];
+                        break;
+                    }
+            }
         }
     }
 }
