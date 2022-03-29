c7b6945daab5ec23c4ddebdad07115c13da33f84
Wydział Wewnętrzny <gamer@mistaken.pl>
2022-03-29 09:31:37 +0200

    support many candies

--- BetterSCP.SCP330/PluginHandler.cs
+++ BetterSCP.SCP330/PluginHandler.cs
@@ -27,7 +27,7 @@ namespace Mistaken.BetterSCP.SCP330
         public override PluginPriority Priority => PluginPriority.Medium;
 
         /// <inheritdoc/>
-        public override Version RequiredExiledVersion => new Version(4, 2, 2);
+        public override Version RequiredExiledVersion => new Version(5, 0, 0);
 
         /// <inheritdoc/>
         public override void OnEnabled()
--- BetterSCP.SCP330/Scp914Patch.cs
+++ BetterSCP.SCP330/Scp914Patch.cs
@@ -5,11 +5,12 @@
 // -----------------------------------------------------------------------
 
 using System.Collections.Generic;
+using System.Linq;
+using Exiled.API.Features;
 using Exiled.API.Features.Items;
 using HarmonyLib;
 using InventorySystem.Items.Pickups;
 using InventorySystem.Items.Usables.Scp330;
-using Mirror;
 using Scp914;
 using UnityEngine;
 
@@ -18,6 +19,16 @@ namespace Mistaken.BetterSCP.SCP330
     [HarmonyPatch(typeof(Scp914Upgrader), nameof(Scp914Upgrader.ProcessPickup))]
     internal class Scp914Patch
     {
+        public const int OneToOneDestroyChance = 35;
+
+        public const int FineDestroyChance = 35;
+        public const int FinePinkChance = 10;
+        public const int FinePinkPillsChance = 50;
+
+        public const int VeryFineDestroyChance = 50;
+        public const int VeryFinePinkChance = 5;
+        public const int VeryFinePinkPillsChance = 30;
+
         public static readonly List<CandyKindID> CandyList = new List<CandyKindID>()
         {
             CandyKindID.Yellow,
@@ -32,82 +43,111 @@ namespace Mistaken.BetterSCP.SCP330
         public static bool Prefix(ItemPickupBase pickup, Vector3 moveVector, Scp914KnobSetting setting)
         {
             if (!(pickup is Scp330Pickup p))
-            {
                 return true;
-            }
 
-            var newPos = pickup.transform.position + moveVector;
-
-            switch (setting)
+            try
             {
-                case Scp914KnobSetting.OneToOne:
+                if (setting == Scp914KnobSetting.Coarse || setting == Scp914KnobSetting.Rough)
+                {
+                    p.DestroySelf();
+                    return false;
+                }
+
+                var newPos = pickup.transform.position + moveVector;
+                foreach (var candy in p.StoredCandies.ToArray())
+                {
+                    switch (setting)
                     {
-                        if (Random.Range(0, 100) <= 35)
-                        {
+                        case Scp914KnobSetting.OneToOne:
+                            {
+                                if (Random.Range(0, 100) < OneToOneDestroyChance)
+                                {
+                                    p.StoredCandies.Remove(candy);
+                                    break;
+                                }
+
+                                var candyType = Scp330Candies.GetRandom();
+                                p.StoredCandies.Remove(candy);
+                                p.StoredCandies.Add(candyType);
+                                break;
+                            }
+
+                        case Scp914KnobSetting.Fine:
+                            {
+                                if (Random.Range(0, 100) < FineDestroyChance)
+                                {
+                                    p.StoredCandies.Remove(candy);
+                                    break;
+                                }
+
+                                int index = CandyList.IndexOf(candy) + 1;
+                                index %= CandyList.Count;
+                                int random = Random.Range(0, 100);
+                                if (CandyList[index] == CandyKindID.Pink)
+                                {
+                                    if (random < FinePinkChance)
+                                        p.StoredCandies.Add(CandyList[index]);
+                                    else if (random >= FinePinkChance && random < FinePinkChance + FinePinkPillsChance)
+                                        new Item(ItemType.Painkillers).Spawn(newPos);
+
+                                    p.StoredCandies.Remove(candy);
+                                    break;
+                                }
+
+                                p.StoredCandies.Remove(candy);
+                                p.StoredCandies.Add(CandyList[index]);
+                                break;
+                            }
+
+                        case Scp914KnobSetting.VeryFine:
+                            {
+                                if (Random.Range(0, 100) < VeryFineDestroyChance)
+                                {
+                                    p.StoredCandies.Remove(candy);
+                                    break;
+                                }
+
+                                int index = CandyList.IndexOf(candy) + 2;
+                                index %= CandyList.Count;
+                                int random = Random.Range(0, 100);
+                                if (CandyList[index] == CandyKindID.Pink)
+                                {
+                                    if (random < VeryFinePinkChance)
+                                        p.StoredCandies.Add(CandyList[index]);
+                                    else if (random >= VeryFinePinkChance && random < VeryFinePinkChance + VeryFinePinkPillsChance)
+                                        new Item(ItemType.Painkillers).Spawn(newPos);
+
+                                    p.StoredCandies.Remove(candy);
+                                    break;
+                                }
+
+                                p.StoredCandies.Remove(candy);
+                                p.StoredCandies.Add(CandyList[index]);
+                                break;
+                            }
+
+                        default:
+                            p.StoredCandies.Remove(candy);
                             break;
-                        }
-
-                        var candyType = Scp330Candies.GetRandom();
-                        p.NetworkExposedCandy = candyType;
-                        break;
                     }
+                }
 
-                case Scp914KnobSetting.Fine:
-                    {
-                        if (Random.Range(0, 100) <= 35)
-                        {
-                            break;
-                        }
+                if (p.StoredCandies.Count == 0)
+                    p.DestroySelf();
+                else
+                {
+                    p.NetworkExposedCandy = p.StoredCandies.Count == 1 ? p.StoredCandies.First() : CandyKindID.None;
+                    p.transform.position += moveVector;
+                    p.RefreshPositionAndRotation();
+                }
 
-                        int index = CandyList.IndexOf(p.NetworkExposedCandy) + 1;
-                        int random = Random.Range(0, 100);
-                        if (CandyList[index] == CandyKindID.Rainbow)
-                        {
-                            if (random >= 10)
-                            {
-                                p.NetworkExposedCandy = CandyList[index];
-                            }
-                            else if (random <= 11 && random >= 50)
-                            {
-                                new Item(ItemType.Painkillers).Spawn(newPos);
-                            }
-
-                            return false;
-                        }
-
-                        p.NetworkExposedCandy = CandyList[index];
-                        break;
-                    }
-
-                case Scp914KnobSetting.VeryFine:
-                    {
-                        if (Random.Range(0, 100) <= 50)
-                        {
-                            break;
-                        }
-
-                        int index = CandyList.IndexOf(p.NetworkExposedCandy) + 1;
-                        int random = Random.Range(0, 100);
-                        if (CandyList[index] == CandyKindID.Rainbow)
-                        {
-                            if (random >= 5)
-                            {
-                                p.NetworkExposedCandy = CandyList[index];
-                            }
-                            else if (random <= 6 && random >= 35)
-                            {
-                                new Item(ItemType.Painkillers).Spawn(newPos);
-                            }
-
-                            return false;
-                        }
-
-                        p.NetworkExposedCandy = CandyList[index];
-                        break;
-                    }
+                return false;
             }
-
-            return false;
+            catch (System.Exception ex)
+            {
+                Log.Error(ex);
+                return false;
+            }
         }
     }
 }
